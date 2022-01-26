c0f309c65addfbd44633a34b040968480f0bf6f2
Saveliy <savisafe@vk.com>
2022-01-26 23:31:12 +0700

    update script

--- script.js
+++ script.js
@@ -25,4 +25,55 @@ function trashOn () {
     out.appendChild(cover)
     console.log('click')
 }
+
 trash.addEventListener('click', trashOn)
+
+const task = document.getElementById('addTask')
+
+function addTask () {
+    const displayNone = document.getElementById('none')
+    displayNone.setAttribute('class', 'container__none')
+
+    const out = document.getElementById('outTask')
+
+    const containerNew = document.createElement('div')
+    containerNew.setAttribute('class', 'container__new')
+
+    const position = document.createElement('div')
+    position.setAttribute('class', 'btn__position')
+    const btnCancel = document.createElement('button')
+    btnCancel.setAttribute('class', 'container__btn')
+    btnCancel.append('Cancel')
+    const btnDone = document.createElement('button')
+    btnDone.setAttribute('class', 'container__btn')
+    btnDone.append('Done')
+    position.append(btnCancel, btnDone)
+
+    const checkbox = document.createElement('div')
+    checkbox.setAttribute('class', 'checkbox')
+    const input = document.createElement('input')
+    input.setAttribute('class', 'checkbox__input')
+    input.setAttribute('type', 'checkbox')
+    input.setAttribute('id', 'checkbox_1')
+    const label = document.createElement('label')
+    label.setAttribute('class', 'checkbox__label')
+    label.setAttribute('for', 'checkbox_1')
+    checkbox.append(input,label)
+
+    const p = document.createElement('p')
+    p.setAttribute('class', 'container__New__input')
+    const inputB = document.createElement('input')
+    inputB.setAttribute('class', 'container__New__position')
+    inputB.setAttribute('size', '22')
+    inputB.setAttribute('placeholder', 'What do you want to do?')
+    p.append(inputB)
+
+    const container = document.createElement('div')
+    container.setAttribute('class', 'checkboxNew_position')
+    container.append(checkbox, p)
+
+    out.append(containerNew, position, container)
+    console.log('click')
+}
+
+task.addEventListener('click', addTask)
