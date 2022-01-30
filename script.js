98ef64c5b6461c50f952ac2ce6fab32d59555477
Saveliy <savisafe@vk.com>
2022-01-30 23:46:09 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-const cover = document.getElementById('trash')
 
+// КНОПКА КОРЗИНЫ
+const cover = document.getElementById('trash')
 cover.addEventListener('click', () => {
     const cover = document.getElementById('cover')
     cover.classList.remove('display-none')
@@ -7,8 +8,8 @@ cover.addEventListener('click', () => {
     container.classList.add('display-none')
 })
 
+// МОДАЛКА "НЕТ"
 const btnCard = document.getElementById('btn-card-no')
-
 btnCard.addEventListener('click', () => {
     const container = document.getElementById('container')
     container.classList.remove('display-none')
@@ -16,8 +17,8 @@ btnCard.addEventListener('click', () => {
     cover.classList.add('display-none')
 })
 
+// КНОПКА "+"
 const addTask = document.getElementById('addTask')
-
 addTask.addEventListener('click', () => {
     const container = document.getElementById('container')
     container.classList.add('display-none')
@@ -25,12 +26,18 @@ addTask.addEventListener('click', () => {
     taskNew.classList.remove('display-none')
 })
 
+// КНОПКА "CANCEL"
 const cancel = document.getElementById('cancel')
-
 cancel.addEventListener('click', () => {
     const taskNew = document.getElementById('taskNew')
     taskNew.classList.add('display-none')
     const container = document.getElementById('container')
     container.classList.remove('display-none')
+})
 
+// ЗАЧЁРКНУТЫЙ ТЕКСТ, КОГДА ЗАДАЧА ВЫПОЛНЕНА
+const taskDelete = document.getElementById('taskDelete')
+taskDelete.addEventListener('click', () => {
+    const addClass = document.getElementById('addClass')
+    addClass.classList.add('task-text-completed')
 })
